5a95610d8ab26f48b1de5869c5e63684e0f9c8a8
Bhumika Ramawat <aakashramawat@Aakashs-MacBook-Pro.local>
2026-06-10 06:39:18 +0530

    Use dynamic port for deployment

--- server.js
+++ server.js
@@ -120,6 +120,8 @@ app.get('/test-imap', async (req, res) => {
     res.send('❌ ' + err.message);
   }
 });
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
